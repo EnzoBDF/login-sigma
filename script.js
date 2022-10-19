4115a6037b40665e0cab82a2ec5dcf4b26ccb034
Bigborg <100381400+EnzoBDF@users.noreply.github.com>
2022-10-19 16:38:30 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,47 @@
+// elementos do DOM
+const inputRegisterUsername = document.getElementById("inputRegisterUsername");
+const inputRegisterPassword = document.getElementById("inputRegisterPassword");
+const inputConfirmPassword = document.getElementById("inputConfirmPassword");
+const inputLoginUsername = document.getElementById("inputLoginUsername");
+const inputLoginPassword = document.getElementById("inputLoginPassword");
+const spanCurrentUser = document.getElementById("spanCurrentUser");
+const btnDesconectar = document.getElementById("btnDesconectar");
+const divLogin = document.getElementById("divLogin");
+
+// lista de usuarios
+const users = [];
+
+function register() {
+    // checa se usuario ja existe
+    if (users.find(obj => obj.username === inputRegisterUsername.value) === undefined) {
+        // checa se senhas sao iguais
+        if(inputRegisterPassword.value === inputConfirmPassword.value) {
+            // empurra objeto de usuário para o array
+            users.push({username: inputRegisterUsername.value,
+                        password: inputRegisterPassword.value}); 
+            alert("USUÁRIO CRIADO");
+        }
+        else alert("SENHAS NÃO COINCIDEM");
+    }
+    else alert("USUÁRIO JÁ EXISTENTE");
+}
+
+function login() {
+    // checa se usuario e senha estao no array
+    if (users.find(obj => (obj.username === inputLoginUsername.value) && 
+                          (obj.password === inputLoginPassword.value))) {
+        // mostra e esconde elementos da UI
+        spanCurrentUser.innerHTML = `Usuário logado: <b>${inputLoginUsername.value}</b>`;
+        btnDesconectar.style.display = "block";
+        divLogin.style.display = "none";
+        alert("LOGIN FEITO COM SUCESSO");
+    }
+    else alert("USUÁRIO OU SENHA INVÁLIDOS");
+}
+
+function logoff() {
+    // mostra e esconde elementos da UI
+    btnDesconectar.style.display = "none";
+    spanCurrentUser.innerHTML = "";
+    divLogin.style.display = "block";
+}
